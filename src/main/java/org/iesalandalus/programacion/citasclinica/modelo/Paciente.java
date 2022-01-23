78d2476d0365791f4a06c045519aef1d702ec0e7
Raúl Herrerías Corral <raulhc1313@gmail.com>
2022-01-23 18:06:04 +0100

    Arreglado Paciente

--- src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Paciente.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Paciente.java
@@ -20,14 +20,14 @@ public class Paciente {
 		setTelefono(telefono);
 	}
 	
-	public Paciente(Paciente paciente) {
+	public Paciente(Paciente pacienteC) {
 		
-		if (paciente == null) {
-			throw new NullPointerException("No se puede copiar un paciente nulo.");
+		if (pacienteC == null) {
+			throw new NullPointerException("ERROR: No es posible copiar un paciente nulo.");
 		}
-		this.nombre = paciente.getNombre(); 
-		this.dni = paciente.getDni();
-		this.telefono = paciente.getTelefono();
+		setNombre(pacienteC.getNombre());
+		setDni(pacienteC.getDni());
+		setTelefono(pacienteC.getTelefono());
 	}
 	
 	public String getNombre() {
@@ -64,9 +64,16 @@ public class Paciente {
 		if(dni == null) {
 			throw new NullPointerException("ERROR: El DNI de un paciente no puede ser nulo o vacío.");
 		}
-		if (comprobarLetraDni(dni) == false) {//Comprueba que es un DNI válido
-			throw new IllegalArgumentException("No es un DNI válido");
+		
+		if (dni.matches(ER_DNI) == false) {
+			throw new IllegalArgumentException("ERROR: No es un DNI válido");
 		}
+		
+		if (comprobarLetraDni(dni) == false) {//Comprueba que es un DNI válido
+			throw new IllegalArgumentException("ERROR: No es un DNI válido");
+		}
+		
+		
 		this.dni = dni;
 	}
 	
@@ -83,18 +90,22 @@ public class Paciente {
 	
 	private Boolean comprobarLetraDni(String dni) {
 		
-		boolean valido;
+		
+		
+		boolean valido = false;
 		
 		Pattern patron;
         Matcher comparador;
         patron = Pattern.compile(ER_DNI);//Se le da valor al patron, en este caso la ER de DNI
-        comparador = patron.matcher(dni);//Se compara el patron con la string dni
+        do {
+        	comparador = patron.matcher(dni);//Se compara el patron con la string dni
+        } while (!comparador.matches());
         
-        char dniLetra = comparador.group(2).charAt(0); //Se pasa la letra de string a char
-        int dniNum;
         
-        dniNum = Integer.parseInt(comparador.group(1));//Se pasan los numeros de string a int
+        char dniLetra = (comparador.group(2)).charAt(0); //Se pasa la letra de string a char
         
+        int dniNum = Integer.parseInt(comparador.group(1));//Se pasan los numeros de string a int
+         
         int resto = dniNum%23;
         
         char[] letras = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
@@ -102,6 +113,7 @@ public class Paciente {
         if(letras[resto] == dniLetra) {
         	valido = true;
         } else valido = false;
+        
 		
 		return valido;
 	}
@@ -112,18 +124,19 @@ public class Paciente {
 	public void setTelefono(String telefono) {
 	
 		if(telefono == null) {
-			throw new NullPointerException("No se puede copiar un telefono nulo.");
+			throw new NullPointerException("ERROR: El teléfono de un paciente no puede ser nulo o vacío.");
 		}
-		
-		if ((Pattern.matches(ER_TELEFONO, telefono)) == false) {
-			throw new IllegalArgumentException("No es un número de teléfono válido");
+			
+		if (telefono.matches(ER_TELEFONO) == false) {
+			throw new IllegalArgumentException("ERROR: No es un número de teléfono válido");
 		}
 		
 		this.telefono = telefono;
 	}
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(dni, nombre, telefono);
+		return Objects.hash(dni);
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -134,11 +147,10 @@ public class Paciente {
 		if (getClass() != obj.getClass())
 			return false;
 		Paciente other = (Paciente) obj;
-		return Objects.equals(dni, other.dni) && Objects.equals(nombre, other.nombre)
-				&& Objects.equals(telefono, other.telefono);
+		return Objects.equals(dni, other.dni);
 	}
 	@Override
 	public String toString() {
-		return String.format("nombre=%s (%s), dni=%s, teléfono=%s", nombre, getIniciales(), dni, telefono);
+		return String.format("Nombre= %s (%s), DNI=%s, Teléfono=%s", nombre, getIniciales(), dni, telefono);
 	}
 }	
